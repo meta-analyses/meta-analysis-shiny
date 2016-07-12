97ed300f9ccccf75cc7e0051c1893b5729ed2339
usr110 <m.aliabbas@gmail.com>
2016-07-12 11:58:25 +0100

    Add minimum input fields for the analysis

--- ui.R
+++ ui.R
@@ -1,17 +1,24 @@
 source("setup.R")
 
+pa_exposure <- c("LTPA", "PTA")
+sub_population <- c("Overall Population",
+                    "Male Population",
+                    "Female Population")
+
 shinyUI(fluidPage(
   list(tags$title(HTML('Meta-Analysis'))),
   useShinyjs(),
   width="100%", height="100%",
   sidebarPanel(
     conditionalPanel(condition="input.conditionedPanels == 1",
-                     selectInput(inputId = "inOutcome", label = "Select Outcome:", choices =  uoutcome$outcome)
+                     selectInput(inputId = "in_outcome", label = "Select Outcome:", choices =  uoutcome$outcome),
+                     selectInput(inputId = "in_PA_exposure", label = "Select Physical Activity Exposure:", choices =  pa_exposure),
+                     radioButtons("in_sub_population", "Population: ", sub_population, inline = TRUE)
     )
   ),
   mainPanel(
     tabsetPanel(
-      tabPanel("Mode Share", value = 1,
+      tabPanel("Analysis", value = 1,
                showOutput("plotAnalysis", "highcharts")
       ),
       
