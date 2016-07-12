c4781f1833b79b33a5338a48cd8621394fbcb24b
usr110 <m.aliabbas@gmail.com>
2016-07-12 11:47:35 +0100

    Read data from the meta-analysis repo and also the list of functions

---
+++ data-processing.R
@@ -0,0 +1,18 @@
+rm (list = ls())
+# Read the data
+data <- read.csv("../meta-analysis/data/09.06_COMBINED DATASET REDUCED.csv", header = T, stringsAsFactors = F, skipNul = TRUE)
+
+data$tot_personyrs <- as.numeric(data$tot_personyrs)
+data[(is.na(data$tot_personyrs)),]$tot_personyrs <- 
+  data[(is.na(data$tot_personyrs)),]$mean_followup * data[(is.na(data$tot_personyrs)),]$n_baseline
+data[(is.na(data$mean_followup)),]$mean_followup <- 
+  data[(is.na(data$mean_followup)),]$tot_personyrs / data[(is.na(data$mean_followup)),]$n_baseline
+
+data$outcome <- trimws(data$outcome)
+
+# Read all the functions
+source("../meta-analysis/all-functions.R")
+
+# Identify unique outcomes
+uoutcome <- data.frame(outcome = as.character(unique(data$outcome)))
+uoutcome$outcome <- as.character(uoutcome$outcome)
