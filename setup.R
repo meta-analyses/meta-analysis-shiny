56bda57a486e43525df3225358ab4faa8e3fdaec
usr110 <m.aliabbas@gmail.com>
2016-07-18 11:07:20 +0100

    Remove unused code

--- setup.R
+++ setup.R
@@ -10,14 +10,5 @@ pkgs <- c(
 # lapply(pkgs, library, character.only = T)
 reqs <- as.numeric(lapply(pkgs, require, character.only = TRUE))
 
-# Committing all the installation packages
-# # Install packages we require
-# if(sum(!reqs) > 0) install.packages(pkgs[!reqs])
-# 
-# if(!require(rCharts)) devtools::install_github("rCharts", "ramnathv")
-# 
-# if(!require(shiny)) devtools::install_github("shiny", "rstudio")
-
+# Load data
 source("data-processing.R")
-# Functions
-#source("functions.R")
