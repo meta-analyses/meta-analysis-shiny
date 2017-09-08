289499c734a21d91ab4124324c5f921868ac40e9
usr110 <m.aliabbas@gmail.com>
2017-09-08 11:54:50 +0100

    Fix a bug to remove single gender specific studies

--- data-processing.R
+++ data-processing.R
@@ -113,15 +113,14 @@ raw_data_gsp_ltpa <- subset(raw_data, pa_domain_subgroup == "LTPA" & (sex_subgro
 for (i in 1:nrow(uoutcome)){
   if (!i %in% c(2,6)){
     dat <- subset(raw_data_gsp_ltpa, outcome == uoutcome$outcome[i])
-    for (j in 1:length(unique(dat$ref_number))){
-      dat1 <- subset(dat, ref_number == (unique(dat$ref_number))[j]) 
+    uid <- unique(dat$ref_number)
+    for (j in 1:length(uid)){
+      dat1 <- subset(dat, ref_number == uid[j]) 
       usexgroups <- unique(dat1$sex_subgroups)
       if (length(usexgroups) == 1){
         # Remove single gender specific studies
-        raw_data_gsp_ltpa <- subset(raw_data_gsp_ltpa, 
-                                    (ref_number == unique(dat1$ref_number) & sex_subgroups == usexgroups) | 
-                                      (ref_number != unique(dat1$ref_number))) 
-        # cat(uoutcome$outcome[i], " - ", unique(dat1$ref_number), " - ", unique(dat1$overall), " - ", unique(dat1$sex_subgroups), "\n")
+        raw_data_gsp_ltpa <- subset(raw_data_gsp_ltpa, (ref_number != uid[j])) 
+        cat("gsp ",uoutcome$outcome[i], " - ", unique(dat1$ref_number), " - ", unique(dat1$overall), " - ", unique(dat1$sex_subgroups), "\n")
       }
     }
   }
@@ -136,8 +135,8 @@ raw_data_tp_ltpa[raw_data_tp_ltpa$overall != 1 & raw_data_tp_ltpa$sex_subgroups 
 raw_data_tp_ltpa <- plyr::arrange(raw_data_tp_ltpa, outcome)
 
 
-raw_data_gsp_ltpa[raw_data_gsp_ltpa$overall != 1 & raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number <- paste(raw_data_gsp_ltpa[raw_data_gsp_ltpa$overall != 1 & raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number, "-1")
-raw_data_gsp_ltpa[raw_data_gsp_ltpa$overall != 1 & raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number <- paste(raw_data_gsp_ltpa[raw_data_gsp_ltpa$overall != 1 & raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number, "-2")
+raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number <- paste(raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 1,]$ref_number, "-1")
+raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number <- paste(raw_data_gsp_ltpa[raw_data_gsp_ltpa$sex_subgroups == 2,]$ref_number, "-2")
 
 raw_data_gsp_ltpa <- plyr::arrange(raw_data_gsp_ltpa, outcome)
 
